ef55170b266b404b4ff8de37608dbf214bf169c8
Vasfi Ölmez <58458944+vasfiolmez@users.noreply.github.com>
2023-12-26 14:28:57 +0300

    entities complete

--- CarRentalManagement.Domain/Entities/Car.cs
+++ CarRentalManagement.Domain/Entities/Car.cs
@@ -19,5 +19,9 @@ namespace CarRentalManagement.Domain.Entities
         public byte Luggage { get; set; }
         public string Fuel { get; set; }
         public string BigImageUrl { get; set;}
+        public List<CarFeature> CarFeatures {  get; set; } 
+        public List<CarDescription> CarDescriptions {  get; set; }
+        public List<CarPricing> CarPricings { get; set; }
+
     }
 }

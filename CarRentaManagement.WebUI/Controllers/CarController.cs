a668570d442fec0a8aabb85017f7a5ca97cbdabe
Vasfi Ölmez <58458944+vasfiolmez@users.noreply.github.com>
2024-03-31 17:26:49 +0300

    contact page finish

--- CarRentaManagement.WebUI/Controllers/CarController.cs
+++ CarRentaManagement.WebUI/Controllers/CarController.cs
@@ -1,27 +1,27 @@
-﻿using CarRentalManagement.Dto.ServiceDtos;
+﻿using CarRentalManagement.Dto.CarDtos;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
 namespace CarRentaManagement.WebUI.Controllers
 {
-    public class ServiceController : Controller
+    public class CarController : Controller
     {
         private readonly IHttpClientFactory _httpClientFactory;
 
-        public ServiceController(IHttpClientFactory httpClientFactory)
+        public CarController(IHttpClientFactory httpClientFactory)
         {
             _httpClientFactory = httpClientFactory;
         }
 
 
-        public async Task <IActionResult> Index()
+        public async Task<IActionResult> Index()
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7056/api/Services");
+            var responseMessage = await client.GetAsync("https://localhost:7056/api/Cars/GetCarWithBrand");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData);
+                var values = JsonConvert.DeserializeObject<List<ResultCarWithBrandsDtos>>(jsonData);
                 return View(values);
             }
             return View();

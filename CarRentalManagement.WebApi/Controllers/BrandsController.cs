059a8fd61ebcd0ebb0d6b23a41d7c74bbbad3fca
Vasfi Ölmez <58458944+vasfiolmez@users.noreply.github.com>
2024-04-09 23:35:45 +0300

    area eklendi

--- CarRentalManagement.WebApi/Controllers/BrandsController.cs
+++ CarRentalManagement.WebApi/Controllers/BrandsController.cs
@@ -42,7 +42,7 @@ namespace CarRentalManagement.WebApi.Controllers
             await _createBrandCommandHandler.Handle(command);
             return Ok("Marka Bilgisi Eklendi");
         }
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public async Task<IActionResult> RemoveBrand(int id)
         {
             await _removeBrandCommandHandler.Handle(new RemoveBrandCommand(id));

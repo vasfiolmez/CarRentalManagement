2bf56104e006fdc5adc4d075bd159fe7751e2ae1
Vasfi Ölmez <58458944+vasfiolmez@users.noreply.github.com>
2024-05-15 15:47:13 +0300

    s

--- CarRentaManagement.WebUI/Areas/Admin/Controllers/AdminCarFeatureDetailController.cs
+++ CarRentaManagement.WebUI/Areas/Admin/Controllers/AdminCarFeatureDetailController.cs
@@ -51,19 +51,41 @@ namespace CarRentaManagement.WebUI.Areas.Admin.Controllers
             return RedirectToAction("Index", "AdminCar");
         }
 
-        [Route("CreateFeatureByCarId")]
+        [Route("CreateFeatureByCarId/{id}")]
         [HttpGet]
-        public async Task<IActionResult> CreateFeatureByCarId()
+        public async Task<IActionResult> CreateFeatureByCarId(int id)
         {
+            ViewBag.Id = id;    
             var client = _httpClientFactory.CreateClient();
             var responseMessage = await client.GetAsync("https://localhost:7056/api/Features");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<ResultFeatureDto>>(jsonData);
+                var values = JsonConvert.DeserializeObject<List<ResultCarFeatureByCarIdDto>>(jsonData);
                 return View(values);
             }
             return View();
+        }
+        [HttpPost]
+        [Route("CreateFeatureByCarId/{id}")]
+        public async Task<IActionResult> CreateFeatureByCarId(List<ResultCarFeatureByCarIdDto> resultCarFeatureByCarIdDto)
+        {
+
+            foreach (var item in resultCarFeatureByCarIdDto)
+            {
+                if (item.Available)
+                {
+                    var client = _httpClientFactory.CreateClient();
+                    await client.GetAsync("https://localhost:7056/api/CarFeatures/CarFeatureChangeAvailableToTrue?id=" + item.CarFeatureID);
+
+                }
+                else
+                {
+                    var client = _httpClientFactory.CreateClient();
+                    await client.GetAsync("https://localhost:7056/api/CarFeatures/CarFeatureChangeAvailableToFalse?id=" + item.CarFeatureID);
+                }
+            }
+            return RedirectToAction("Index", "AdminCar");
         }
     }
 }
